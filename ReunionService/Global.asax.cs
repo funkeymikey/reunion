5e913cbf4925c88962bfe0f1210ce1cb283fa4ce
Mike Kozelsky <mike.kozelsky@gmail.com>
2014-01-21 11:20:51 -0500

    updated packages & added morinreunion.com to the CORS whitelist

--- ReunionService/Global.asax.cs
+++ ReunionService/Global.asax.cs
@@ -10,7 +10,7 @@ namespace EmailService
     protected void Application_Start()
     {
       // Enable CORS
-      EnableCorsAttribute cors = new EnableCorsAttribute("http://www.morinreunion.com,http://localhost:52529,http://morinreunion.azurewebsites.net", "*", "*");
+      EnableCorsAttribute cors = new EnableCorsAttribute("http://www.morinreunion.com,http://morinreunion.com,http://localhost:52529,http://morinreunion.azurewebsites.net", "*", "*");
       GlobalConfiguration.Configuration.EnableCors(cors);
 
       //camelCase our outgoing javascript
