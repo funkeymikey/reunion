96080fd0adc028547c37628aa63f44f388906578
Mike Kozelsky <mike.kozelsky@gmail.com>
2014-07-03 10:03:09 -0400

    updated to use SSL

--- FlickrUtilities/FlickrApiLocation.cs
+++ FlickrUtilities/FlickrApiLocation.cs
@@ -4,8 +4,8 @@ namespace FlickrUtilities
   public class FlickrApiLocation
   {
     
-    public static readonly FlickrApiLocation MAIN = new FlickrApiLocation("http://api.flickr.com/services/rest/");
-    public static readonly FlickrApiLocation UPLOAD = new FlickrApiLocation("http://up.flickr.com/services/upload/");
+    public static readonly FlickrApiLocation MAIN = new FlickrApiLocation("https://api.flickr.com/services/rest/");
+    public static readonly FlickrApiLocation UPLOAD = new FlickrApiLocation("https://up.flickr.com/services/upload/");
 
     public string Uri { get; private set; }
     private FlickrApiLocation(string uri){
